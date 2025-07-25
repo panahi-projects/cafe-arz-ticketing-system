b8d420216f9b3a45326ebd1befcebc7cac8cc3cf
SAEED.PANAHI <panahi.projects@gmail.com>
2025-07-25 16:11:25 +0330

    ui: perform page titles in terms of responsiveness

--- components/PageTitle.tsx
+++ components/PageTitle.tsx
@@ -3,17 +3,23 @@
 
 import { menuItems } from "@/configs";
 import { getPageTitle } from "@/lib/utils";
-import { Typography } from "@mui/material";
+import { Typography, useMediaQuery } from "@mui/material";
 import { usePathname } from "next/navigation";
 
 export const PageTitle = () => {
   const pathname = usePathname();
+  const isMobile = useMediaQuery("(max-width:768px)");
   const title = getPageTitle(pathname, menuItems);
 
   if (!title) return null;
 
   return (
-    <Typography variant="h5" component="h1" fontWeight={500} gutterBottom>
+    <Typography
+      variant={isMobile ? "h6" : "h5"}
+      component="h1"
+      fontWeight={500}
+      gutterBottom
+    >
       {title}
     </Typography>
   );
